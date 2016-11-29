5e3c1c1b037331982d7b6d58be4c002cd46d198a
虾哥哥 <jfk_jin@sina.com>
2016-11-29 23:23:36 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,40 @@
+from services import *
+from xbmcgui import *
+
+Meiju = MeiJu()
+
+
+def search(page, **kwargs):
+    c_list = Meiju.search(page, PAGE_ROWS, **kwargs)
+    for one in c_list["data"]["results"]:
+        item = ListItem(**{
+            'label': one.get("title"),
+            'path': plugin.url_for("detail", seasonId=one.get("id")),
+            'icon': one["cover"],
+            'thumbnail': one["cover"],
+        })
+        item.set_info("video", {"plot": one.get("brief", ""),
+                                "rating ": float(one["score"]),
+                                "genre": one["cat"],
+                                "season": one["seasonNo"]})
+        item._listitem.setArt({"poster": one["cover"]})
+        item.set_is_playable(False)
+        yield item
+
+
+def get_album(albumId):
+    c_list = Meiju.get_album(albumId)
+    for one in c_list["data"]["results"]:
+        item = ListItem(**{
+            'label': one.get("title"),
+            'path': plugin.url_for("detail", seasonId=one.get("id")),
+            'icon': one["cover"],
+            'thumbnail': one["cover"],
+        })
+        item.set_info("video", {"plot": one.get("brief", ""),
+                                "rating ": float(one["score"]),
+                                "genre": one["cat"],
+                                "season": one["seasonNo"]})
+        item._listitem.setArt({"poster": one["cover"]})
+        item.set_is_playable(False)
+        yield item
